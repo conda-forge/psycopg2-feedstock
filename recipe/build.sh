84741eed3512faec66ddf73e054f605057da7828
Isuru Fernando <isuruf@gmail.com>
2020-12-24 15:13:20 -0600

    Fix CFLAGS and remove cruft

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,16 +4,7 @@ export LDFLAGS="${LDFLAGS} -L$PREFIX/lib -lssl"
 
 if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
     cp $BUILD_PREFIX/bin/pg_config $PREFIX/bin/pg_config
+    export CFLAGS="${CFLAGS} -I$PREFIX/include -I$PREFIX/include/server"
 fi
 
 $PYTHON -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
-
-if [[ "$target_platform" == osx-* ]]; then
-     PG_LIB=$(pg_config --libdir)
-     for LIBRARY in $(find ${SP_DIR}/${PKG_NAME} -name "*.so");
-     do
-        ${OTOOL:-otool} -L $LIBRARY
-        ${INSTALL_NAME_TOOL:-install_name_tool} -change libssl.1.0.0.dylib @rpath/libssl.1.0.0.dylib $LIBRARY
-        ${INSTALL_NAME_TOOL:-install_name_tool} -change libcrypto.1.0.0.dylib @rpath/libcrypto.1.0.0.dylib $LIBRARY
-     done
-fi
