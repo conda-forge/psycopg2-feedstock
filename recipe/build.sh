cabfcd4f0fcec217b27fea0a1eaae26da4399308
Isuru Fernando <isuruf@gmail.com>
2020-12-24 14:58:11 -0600

    copy pg_config and cleanup

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,13 +2,18 @@
 
 export LDFLAGS="${LDFLAGS} -L$PREFIX/lib -lssl"
 
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
+    cp $BUILD_PREFIX/bin/pg_config $PREFIX/bin/pg_config
+fi
+
 $PYTHON -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
 
-if [[ $(uname) == 'Darwin' ]]; then
+if [[ "$target_platform" == osx-* ]]; then
      PG_LIB=$(pg_config --libdir)
      for LIBRARY in $(find ${SP_DIR}/${PKG_NAME} -name "*.so");
      do
-        install_name_tool -change libssl.1.0.0.dylib @rpath/libssl.1.0.0.dylib $LIBRARY
-        install_name_tool -change libcrypto.1.0.0.dylib @rpath/libcrypto.1.0.0.dylib $LIBRARY
+        ${OTOOL:-otool} -L $LIBRARY
+        ${INSTALL_NAME_TOOL:-install_name_tool} -change libssl.1.0.0.dylib @rpath/libssl.1.0.0.dylib $LIBRARY
+        ${INSTALL_NAME_TOOL:-install_name_tool} -change libcrypto.1.0.0.dylib @rpath/libcrypto.1.0.0.dylib $LIBRARY
      done
 fi
